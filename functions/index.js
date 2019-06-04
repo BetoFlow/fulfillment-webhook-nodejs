ac73b4d6965aa8935bd45e43d424f4ddb3328e73
ingmiguens <ing.miguens@gmail.com>
2019-06-04 00:53:45 -0300

    Se cargaron los datos de drseedcare

--- functions/index.js
+++ functions/index.js
@@ -1,73 +1,204 @@
-// Copyright 2017, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS IS' BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Dialogflow fulfillment getting started guide:
-// https://dialogflow.com/docs/how-tos/getting-started-fulfillment
-
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
-
+ 
+const {google} = require('googleapis');
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
+const {Text, Card, Image, Suggestion, Payload} = require('dialogflow-fulfillment');
+const {dialogflow, BasicCard, SimpleResponse} = require('actions-on-google');
+const admin = require('firebase-admin');
+const request = require('request');
 
+const app = dialogflow({
+  debug: true
+});
+
+// Do common tasks for each intent invocation
+app.middleware((conv, framework) => {
+  console.log(`Intent=${conv.intent}`);
+  console.log(`Type=${conv.input.type}`);
+  // Determine if the user input is by voice
+  conv.voice = conv.input.type === 'VOICE';
+  if (!(conv.intent === 'Default Fallback Intent' || conv.intent === 'No-input')) {
+    // Reset the fallback counter for error handling
+    conv.data.fallbackCount = 0;
+  }
+});
+ 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
+const timeZone = 'America/Argentina/Buenos_Aires';  // Change it to your time zone
+const timeZoneOffset = '-03:00';         // Change it to your time zone offset
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+	const agent = new WebhookClient({ request, response });
+	console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+	console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  function welcome (agent) {
-    agent.add(`Welcome to my agent!`);
-  }
+	function welcome(agent) {
+		agent.add(`Welcome to my agent!`);
+		}
 
-  function fallback (agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
+	function fallback(agent) {
+		agent.add(`I didn't understand`);
+		agent.add(`I'm sorry, can you try again?`);
+		}
 
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase inline editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://dialogflow.com/images/api_home_laptop.svg',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://docs.dialogflow.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
+	app.intent('Game', (conv, {artist}) => {
+		conv.ask(new SimpleResponse({
+			speech: "Hey, this is a simple response, okay?",
+			text: "Hey, this is a simple response, okay?",
+		}));
+		conv.ask(new BasicCard({
+		  text: `This is a basic card.  Text in a basic card can include "quotes" and
+		  most other unicode characters including emoji 📱.  Basic cards also support
+		  some markdown formatting like *emphasis* or _italics_, **strong** or
+		  __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other
+		  things like line  \nbreaks`, // Note the two spaces before '\n' required for
+									   // a line break to be rendered in the card.
+		  subtitle: 'This is a subtitle',
+		  title: 'Title: this is a title',
+		  display: 'CROPPED',
+		}));
+	});
 
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!'); // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
+	function readFromDbEs (agent) {
+	// Get the database collection 'menues' and document 'comidas'
+	var nombre = agent.parameters.product !== ''? agent.parameters.product : 'noName';
+	console.log(nombre);
+	const dialogflowAgentDoc = db.collection('FirstCollection').doc(nombre);
+	// Get the value of 'entry' in the document and send it to the user
+	return dialogflowAgentDoc.get()
+		.then(doc => {
+			if (!doc.exists) {
+			  agent.add('No tengo datos de la comida solicitada, ¿te puedo ayudar con algo más?');
+			} 
+			else {
+				//agent.add(doc.data().composition_speech);
+				
+				/*agent.add(new Card({
+							title: doc.data().nombre,
+							//imageUrl: doc.data().imagenUrl,
+							text: doc.data().descripcion,
+							//buttonText: 'Linkedin Profile',
+							//buttonUrl: doc.data().lDUrl
+							}));*/
+			  
+				let conv = agent.conv();
+				conv.ask(new SimpleResponse({
+				  speech: doc.data().composition_speech + ' ¿Tenés otra consulta?',
+				  text: doc.data().composition_text,
+				  }));
+				agent.add(conv);
+				//agent.add('¿Tenés otra consulta?');
+			}
+			return Promise.resolve('Read complete');
+		})
+		.catch(() => {
+			agent.add('Hubo un error intentando leer la base de datos');
+		});
+	}
+	
+	function preparation (agent) {
+		// Get the database collection 'menues' and document 'comidas'
+		var nombre = agent.parameters.product !== ''? agent.parameters.product : 'noName';
+		console.log(nombre);
+		const dialogflowAgentDoc = db.collection('FirstCollection').doc(nombre);
+		// Get the value of 'entry' in the document and send it to the user
+		return dialogflowAgentDoc.get()
+			.then(doc => {
+				if (!doc.exists) {
+				  agent.add('No tengo datos de la comida solicitada, ¿te puedo ayudar con algo más?');
+				} 
+				else {
+					//agent.add(doc.data().composition_speech);
+					
+					/*agent.add(new Card({
+								title: doc.data().nombre,
+								//imageUrl: doc.data().imagenUrl,
+								text: doc.data().descripcion,
+								//buttonText: 'Linkedin Profile',
+								//buttonUrl: doc.data().lDUrl
+								}));*/
+                  
+                    let conv = agent.conv();
+                    conv.ask(new SimpleResponse({
+                      speech: doc.data().preparation_speech + ' ¿Tenés otra consulta?',
+                      text: doc.data().preparation_text,
+                      }));
+                    agent.add(conv);
+					//agent.add('¿Tenés otra consulta?');
+				}
+				return Promise.resolve('Read complete');
+			})
+			.catch(() => {
+				agent.add('Hubo un error intentando leer la base de datos');
+			});
+	}
+	
+	
+	function doses (agent) {
+		// Get the database collection 'menues' and snapshot.docsument 'comidas'
+		var bugs = agent.parameters.bugs !== ''? agent.parameters.bugs : 'noName';
+		var Sowing = agent.parameters.Sowing !== ''? agent.parameters.Sowing : 'noName';
+		console.log(bugs);
+		const dialogflowAgentDoc = db.collection('FirstCollection');
+		// Get the value of 'entry' in the snapshot.docsument and send it to the user
+		return dialogflowAgentDoc.where('disease_speech', '==', bugs).where('crops', '==', Sowing).get()
+			.then(snapshot => {
+				if (!snapshot.docs[0].exists) {
+				  agent.add('No tengo datos de la info solicitada, ¿te puedo ayudar con algo más?');
+				} 
+				else {                  
+                    let conv = agent.conv();
+                    conv.ask(new SimpleResponse({
+                      speech: 'Te recomiendo utilizar '+ snapshot.docs[0].data().name + ' en la siguiente dosis: ' + snapshot.docs[0].data().dose_speech + ' ¿Tenés otra consulta?',
+                      text: snapshot.docs[0].data().dose_speech,
+                      }));
+                    agent.add(conv);
+				}
+				//return Promise.resolve('Read complete');
+			})
+			.catch(() => {
+				agent.add('Hubo un error intentando leer la base de datos');
+			});
+	}	
+  
+  
+	function Game2(agent) {
+		const ssml= 
+			`<speak>
+				<par>
+					<media>
+						<audio src="https://actions.google.com/sounds/v1/animals/afternoon_crickets_long.ogg"/>
+					</media>
+					<media>
+						<prosody> Let's go to play! </prosody>
+					</media>
+				</par>
+			</speak>`;
+		let conv = agent.conv();
+    conv.close(ssml);
+    agent.add(conv);
 
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-  // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
-  agent.handleRequest(intentMap);
+		//agent.add(`What is the mean of P in P A S?`);
+		//agent.add(new Suggestion(`Products`));
+		//agent.add(new Suggestion(`Potatoes`));
+		//agent.context.set({ name: 'Trivia', lifespan: 2, parameters: { city: 'Game' }});
+
+		}
+	
+
+	let intentMap = new Map();
+	intentMap.set('Default Welcome Intent', welcome);
+	intentMap.set('Default Fallback Intent', fallback);
+	intentMap.set('Game', Game2);
+  intentMap.set('productComposition', readFromDbEs);
+	intentMap.set('preparation', preparation);
+	intentMap.set('doses', doses);
+	agent.handleRequest(intentMap);
 });
